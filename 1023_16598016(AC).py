a610743a3c5c89c7cc87ce92d6da3e03b673a82c
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2021-01-08 10:06:02 +0900

    Create 1023_16598016(AC).py

---
+++ 1023_16598016(AC).py
@@ -0,0 +1,14 @@
+﻿n = float(input())
+i = int(n) # 정수 부분
+f = n - i # 실수 부분 6자리까지 
+print(i)
+if (f == 0):
+    print(int(f))
+f = '%.6f'%f
+f = f.replace('0','')
+f = f.replace('.','')
+if ('-' in f):
+    f = f.replace('-','')
+print(f)
+
+
