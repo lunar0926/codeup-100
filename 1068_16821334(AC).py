1d50308ac73a55f942fb2d2c80a04c9f71d2f72f
Jaehyeong Park <lunar0926@gmail.com>
2021-01-26 16:02:04 +0900

    Create 1068_16821334(AC).py

---
+++ 1068_16821334(AC).py
@@ -0,0 +1,10 @@
+﻿score = int(input())
+if score >= 90 and score <= 100:
+    print('A')
+elif score >= 70 and score <= 89:
+    print('B')
+elif score >= 40 and score <= 69:
+    print('C')
+else:
+    print('D')
+    
