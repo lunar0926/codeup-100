acac701268ccd9376d6e491ee2aa24f6a4bfa7bb
Jaehyeong Park <lunar0926@gmail.com>
2021-01-29 11:36:12 +0900

    Create 1083_16830598(AC).py

---
+++ 1083_16830598(AC).py
@@ -0,0 +1,10 @@
+﻿'''
+조건문으로 3, 6, 9에 해당될 때 'X'를 출력
+
+'''
+n = int(input())
+for i in range(1, n+1):
+    if i == 3 or i == 6 or i == 9:
+        print('X', end = ' ')
+    else:
+        print(i, end =' ')
