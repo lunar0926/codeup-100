4e81416258ea7484ee3243fcba46588e7503a5d1
Jaehyeong Park <lunar0926@gmail.com>
2021-01-29 11:36:07 +0900

    Create 1080_16830285(AC).py

---
+++ 1080_16830285(AC).py
@@ -0,0 +1,15 @@
+﻿'''
+입력값 n
+1부터 n까지 더하기 sum 
+반복문 i in range(n)
+
+if sum >= n:
+    print(i)
+'''
+n = int(input())
+sum = 0
+for i in range(1, n):
+    sum += i
+    if sum >= n:
+        print(i)
+        break
