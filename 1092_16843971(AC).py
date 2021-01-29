c6f6bff64a4f30fe05d0a0991f09fced9b693ad5
Jaehyeong Park <lunar0926@gmail.com>
2021-01-29 11:36:32 +0900

    Create 1092_16843971(AC).py

---
+++ 1092_16843971(AC).py
@@ -0,0 +1,17 @@
+﻿'''
+최소공배수 활용
+방문주기 문자열을 나눠서 int()
+
+셋 다 같은 날에 모이는 것. 최소공배수가 되는 날. 
+a,b,c로 n이 동시에 나누어 떨어지는 수 
+'''
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+n = 0
+while(True):
+    n += 1
+    if (n % a == 0 and n % b == 0 and n % c == 0):
+        print(n)
+        break
