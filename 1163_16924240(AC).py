f051b9b52b98984c8c9fdca2c07a957869285c27
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2021-02-04 15:45:29 +0900

    Create 1163_16924240(AC).py

---
+++ 1163_16924240(AC).py
@@ -0,0 +1,23 @@
+﻿# 정수를 문자열로 바꿔서 마지막 자리가 '0'인지를 조건식으로 확인
+y, m, d = map(int, input().split())
+result = y + m + d
+s = str(result)
+if len(s) == 5:
+    hundred = int(s[2])
+    if hundred % 2 == 0:
+        print('대박')
+    else:
+        print('그럭저럭')
+elif len(s) == 4:
+    hundred = int(s[1])
+    if hundred % 2 == 0:
+        print('대박')
+    else:
+        print('그럭저럭')
+elif len(s) == 3:
+    hundred = int(s[0])
+    if hundred % 2 == 0:
+        print('대박')
+    else:
+        print('그럭저럭')
+
