c00809f11117aa61af808b48216cf367fca5d032
Jaehyeong Park <lunar0926@gmail.com>
2021-02-24 21:43:47 +0900

    Add files via upload

---
+++ 1283_17144143(AC).py
@@ -0,0 +1,17 @@
+﻿money = int(input())
+day = int(input())
+percent = list(map(int, input().split()))
+result = money
+for i in range(day):
+    result += float(result) * float(percent[i]) / 100.0
+earning = result - money # 순수익 = 최종 값 - 투자한 액수
+earning = round(earning)
+print(earning)
+if earning > 0:
+    print('good')
+elif earning == 0:
+    print('same')
+else:
+    print('bad')
+
+
